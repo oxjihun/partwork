13359c04b8a7676d543b9f0c3ea72e797c2cc70e
Jihun Park <oxjihun9@gmail.com>
2023-03-13 15:09:09 +0900

    모임 직전 커밋

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-# 0부터 n까지 number of partitions 반환 3월 12일에 작성함 12쪽 Euler의 Corollary 이용
+# 0부터 n까지 분할수가 담긴 리스트를 반환한다.
+# 'The Theory of Partitions' Corollary 1.8 (Euler)
 def list_p(n):
     memo = [1] + [0] * n
     for i in range(1, n + 1):
@@ -14,14 +15,13 @@ def list_p(n):
     return memo
 
 
-# parts 수가 m개 이하 (혹은 각 parts가 m이하) / 위에 오일러 아이디어, 생성함수의 역 보고 생각. 이것도 3월 12일 작성
+# 0부터 n까지 분할수 (단, part가 m개 이하)가 담긴 리스트를 반환한다.
+# (1-q) * ... * (1-q^m)이라는 다항식을 먼저 계산한 후 생성함수 계수 비교
 def list_p_with_bound(n, m):
-    # build polynomial (1-q) * ... * (1-q^m)
     pol = [1] + [0] * ((m * (m + 1)) // 2)
     for t in range(1, m + 1):
         for s in range((t * (t - 1)) // 2, -1, -1):
             pol[t + s] -= pol[s]
-    # compute
     memo = [0] * (n + 1)
     memo[0] = 1
     for i in range(1, n + 1):
@@ -30,33 +30,34 @@ def list_p_with_bound(n, m):
     return memo
 
 
-lookup_table = [None] + [list_p_with_bound(1000, m) for m in range(1, 11)]  # 이 테이블의 m열이
+lookup_table = [None] + [list_p_with_bound(1000, m) for m in range(1, 31)]  # 참조용 리스트
+print("Built lookup table.")
 
 
+# 요주의 값
 def objective(n, a, b):
     if a == 1:
         return lookup_table[b][n]
     s = 0
-    for i in range(min(n // a, b) + 1):
+    for i in range(n // a + 1):
         s += lookup_table[b][i] * objective(n - a * i, a - 1, b)
     return s
 
 
-a = 1
-for b in range(1, 11):
-    print(objective(a * b, a, b))  # 예상한 결과
-
-a = 2
-for b in range(1, 11):
-    print(objective(a * b, a, b))  # OEIS에 없다
-
-a = 3
-for b in range(1, 11):
-    print(objective(a * b, a, b))  # OEIS에 없다
-
-a = 4
-for b in range(1, 11):
-    print(objective(a * b, a, b))  # OEIS에 없다 어 잠깐 왜 시작값이 4지
-
-print(objective(4, 1, 4))
-print(objective(4, 4, 1))
+# 값 출력 (Technical)
+N = 10
+objective_table = [[None] * (N + 1) for _ in range(N + 1)]
+for a in range(1, N + 1):
+    for b in range(1, N + 1):
+        objective_table[a][b] = objective(a * b, a, b)
+for a in range(1, N + 1):
+    print(
+        " ".join(
+            [
+                ("{:%dd}" % (len(str(objective_table[-1][b])))).format(
+                    objective_table[a][b]
+                )
+                for b in range(1, N + 1)
+            ]
+        )
+    )
