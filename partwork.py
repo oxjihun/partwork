14f968a01079a8b0e1ca874c0a60e1c46dbba46d
Jihun Park <oxjihun9@gmail.com>
2023-03-26 21:21:31 +0900

    .gitignore 추가, parts_rec으로 이름 변경, Pol 클래스 getitem 추가

--- partwork.py
+++ partwork.py
@@ -9,11 +9,17 @@ class Pol:
     def __len__(self):  # 차수 반환
         return len(self.coef) - 1
 
+    def __getitem__(self, key):
+        return self.coef[key]
+
+    def __str__(self):
+        return str(self.coef)
+
     def __mul__(self, other):
         L = [0] * (1 + len(self) + len(other))
         for i in range(1 + len(self)):
             for j in range(1 + len(other)):
-                L[i + j] += self.coef[i] * other.coef[j]
+                L[i + j] += self[i] * other[j]
         return Pol(L)
 
 
@@ -23,30 +29,32 @@ class Pol:
 def p(n):
     if n == 0:
         return 1
-    ans = 0
+    result = 0
     j, sgn = 1, -1
     while True:
         value = n - (j * (3 * j + sgn)) // 2
         if value >= 0:
-            ans -= ((-1) ** j) * p(value)
+            result -= ((-1) ** j) * p(value)
         else:
             break
         j += sgn == 1
         sgn *= -1
-    return ans
+    return result
 
 
 # 0부터 n까지 분할수 (단, part가 m개 이하)가 담긴 리스트를 반환한다.
 # (1-q) * ... * (1-q^b)이라는 다항식을 먼저 계산한 후 생성함수 계수 비교
 @cache
 def p_with_bound(n, b):
+    if n == 0:
+        return 1
     pol = Pol([1])
     for t in range(1, b + 1):
         pol *= Pol([1] + [0] * (t - 1) + [-1])
-    ans = 0
-    for j in range(1, min(len(pol), n + 1)):
-        ans -= pol[j] * p_with_bound[b][n - j]
-    return ans
+    result = 0
+    for j in range(1, min(len(pol), n) + 1):
+        result -= pol[j] * p_with_bound(n - j, b)
+    return result
 
 
 # 요주의 값
@@ -57,39 +65,6 @@ def p_rec(n, a, b):
     for i in range(n // a + 1):
         s += p_with_bound(i, b) * p_rec(n - a * i, a - 1, b)
     return s
-
-
-# 값 출력 (Technical)
-
-import math
-
-N = 10
-p_rec_table = [[None] * (N + 1) for _ in range(N + 1)]
-for a in range(1, N + 1):
-    for b in range(1, N + 1):
-        p_rec_table[a][b] = p_rec(a * b, a, b)
-        """int(
-            (
-                math.log(
-                    4 * math.sqrt(3) * (a * b) * p_rec(a * b, a, b)
-                )  # a+b는 왠지 아닌 것 같다
-                / (math.pi * math.sqrt(2 / 3))
-            )
-            ** 2
-            / ((a * b) / 1000)
-        )
-        """
-for a in range(1, N + 1):
-    print(
-        " ".join(
-            [
-                ("{:%dd}" % (len(str(p_rec_table[-1][b])))).format(p_rec_table[a][b])
-                for b in range(1, N + 1)
-            ]
-        )
-    )
-
-# 3월 13일
 
 
 def parts_with_bound(n, b):
@@ -111,28 +86,15 @@ def parts(n):
     return parts_with_bound(n, n)
 
 
-def p_rec_parts(n, a, b):
+def parts_rec(n, a, b):
     if a == 1:
         L = parts_with_bound(n, b)
         return [[(1, part) for part in partition] for partition in L]
     result = []
     for i in range(n // a, -1, -1):  # 큰 값부터
         L = parts_with_bound(i, b)
-        R = p_rec_parts(n - a * i, a - 1, b)
+        R = parts_rec(n - a * i, a - 1, b)
         for l in L:
             for r in R:
                 result.append(list(map(lambda p: (a, p), l)) + r)
     return result
-
-
-# print(parts(5))
-for _ in p_rec_parts(9, 3, 3):
-    print(_)
-
-
-def check_asym_with_desmos(n):
-    return list(enumerate(list_p(n)))
-
-
-# print(*check_asym_with_desmos(100))
-# \frac{1}{4x\sqrt{3}}e^{\pi\sqrt{\frac{2}{3}x}} desmos로 그려보니 잘 맞는다
--- test.py
+++ test.py
@@ -1,1 +1,32 @@
 import partwork
+
+
+# 값 출력 (Technical)
+
+import math
+
+N = 10
+p_rec_table = [[None] * (N + 1) for _ in range(N + 1)]
+for a in range(1, N + 1):
+    for b in range(1, N + 1):
+        p_rec_table[a][b] = partwork.p_rec(a * b, a, b)
+        """int(
+            (
+                math.log(
+                    4 * math.sqrt(3) * (a * b) * p_rec(a * b, a, b)
+                )  # a+b는 왠지 아닌 것 같다
+                / (math.pi * math.sqrt(2 / 3))
+            )
+            ** 2
+            / ((a * b) / 1000)
+        )
+        """
+for a in range(1, N + 1):
+    print(
+        " ".join(
+            [
+                ("{:%dd}" % (len(str(p_rec_table[-1][b])))).format(p_rec_table[a][b])
+                for b in range(1, N + 1)
+            ]
+        )
+    )
